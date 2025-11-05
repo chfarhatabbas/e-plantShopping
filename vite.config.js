e507af9b22fc13ee9f9dcd94ebeaac47b96418ed
Farhat Abbas <chfarhatabbas@outlook.com>
2025-11-05 22:57:51 +0500

    name changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/shoppingreact",
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'  // if you're using React
+
 export default defineConfig({
-  base: "/shoppingreact",
-  plugins: [react()],
+  plugins: [ react() ],
+  base: '/e-plantShopping/',  // <-- your repo name, exactly
 })
