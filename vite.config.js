cc65719d1bf102546d567485916e9ca69827b5cb
Farhat Abbas <chfarhatabbas@outlook.com>
2025-11-06 00:28:40 +0500

    small change in index

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   base: "/shoppingreact",
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  // if you're using React
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [ react() ],
-  base: '/e-plantShopping/',  // <-- your repo name, exactly
+  plugins: [react()],
+  base: '/e-plantShopping/',   // <-- IMPORTANT
 })
